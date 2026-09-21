b056a2bd206fa136dba5ab8084fb8938b87045f2
Federal-Sedinam <federalsedinam@gmail.com>
2026-09-21 23:32:41 +0100

    create home page

---
+++ src/services/api.ts
@@ -0,0 +1,38 @@
+
+const API_KEY = import.meta.env.VITE_API_KEY
+const BASE_URL = import.meta.env.VITE_BASE_URL
+
+export interface MovieResult {
+  adult: boolean;
+  backdrop_path: string;
+  genre_ids: number[];
+  id: number;
+  title: string;
+  original_language: string;
+  original_title: string;
+  overview: string;
+  popularity: number;
+  poster_path: string;
+  release_date: string;
+  softcore: boolean;
+  video: boolean;
+  vote_average: number;
+  vote_count: number;
+//   url: string;
+}
+
+export const getPopularMovies = async (): Promise<MovieResult[]> => {
+  const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
+  const data = await response.json();
+  return data.results;
+};
+
+export const searchMovies = async (query: string): Promise<MovieResult[]> => {
+  const response = await fetch(
+    `${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(
+      query,
+    )}`,
+  );
+  const data = await response.json();
+  return data.results;
+};
